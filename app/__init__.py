b555c9632a25123112f7ec6931e3f0f0d4babd56
amruthawme <152927489+amruthawme@users.noreply.github.com>
2023-12-04 16:04:26 -0600

    Delete app directory

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__, static_folder='static', static_url_path='/static',template_folder='templates')
-# Set a secret key for session management
-app.secret_key = 'wme2023'
-
-from app import views
